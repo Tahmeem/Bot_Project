20482fc2661827af561eb2c25104cf3ca945b20c
Tahmeem <tahmeemmahedi@gmail.com>
2021-02-17 19:48:39 -0500

    Can show roles available and add suggestions to a csv

--- DiscordBot.py
+++ DiscordBot.py
@@ -34,8 +34,28 @@ async def on_member_join(member): #When a member joins
 async def on_message(message): #When users sends message
     if message.author == client.user: #To make sure its not a message by bot
         return
+    guild = discord.utils.get(client.guilds, name=Guild)  # Obtains the correct server
+    if message.content == "Dimple show roles available":
+        roles = await guild.fetch_roles()
+        for role in roles:
+            if role.name == '@everyone':
+                continue
+            else:
+                await message.channel.send("`-" + role.name + "`")
+                time.sleep(1)
 
-    guild = discord.utils.get(client.guilds, name=Guild) #Obtains the correct server
+    Word_check = message.content.split(" ")
+    Checker = discord.utils.find(lambda l: l == "!Suggestion", Word_check)
+    if Checker != None:
+        Word_check.pop(0)
+        Word_check = " ".join(Word_check)
+        print(Word_check)
+        file = open('Suggestion.csv', 'a')
+        file.write(f'\n{message.author},{Word_check}')
+        file.close()
+        await message.channel.send("Thank you! Dimple has received your message.")
+
+
     member_list = ["`Server members are:`"]
     for member in guild.members: #Loop stores members in list
         if member.bot: #Adds bot to bot members
@@ -58,6 +78,7 @@ async def on_message(message): #When users sends message
 -give server info if you say `Dimple give me server info`
 -Tell you how I feel if you say `How are you feeling Dimple?`
 -Answer yes/no question if you ask with `Listen` included and add `?` at the end of your question
+-Show roles available if you say `Dimple show roles available`
 """
         await message.channel.send(help_options)
 
@@ -83,4 +104,6 @@ async def on_message(message): #When users sends message
         elif locateListen != None and locateQuestionMark == None: #If listen is said
             fixPunctuationResponse = "Dimple either doesn't see a question or question mark.\nDon't mess with me!"
             await message.channel.send(fixPunctuationResponse)
+
+
 client.run(botTOKEN) #Makes the bot run and connects to discord
